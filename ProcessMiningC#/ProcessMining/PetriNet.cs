f79670d782d4ab7984415da93bf0421c0d314f64
ndobrev <ndobrevgw@gmail.com>
2019-11-17 20:54:00 +0100

    Dijkstra implementation [not tested]

--- ProcessMiningC#/ProcessMining/PetriNet.cs
+++ ProcessMiningC#/ProcessMining/PetriNet.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace ProcessMining
 {
     class Node
     {
-        
         private Dictionary<Node, bool> parentActivationRequest;
 
         public readonly string name;
@@ -57,6 +58,8 @@ namespace ProcessMining
         private Dictionary<int, Node> _places;
         private Dictionary<int, Node> _transitions;
         private Dictionary<string, int> _transitionsNameToId;
+        private int _sinkPlaceId;
+        private int _entrancePlaceId;
 
 
 
@@ -65,6 +68,38 @@ namespace ProcessMining
             _places = new Dictionary<int, Node>(){};
             _transitions = new Dictionary<int, Node>(){};
             _transitionsNameToId = new Dictionary<string, int>(){};
+            _sinkPlaceId = 1;
+            _entrancePlaceId = 1;
+        }
+
+        public int GetSinkPlace()
+        {
+            if (1 == this._sinkPlaceId)
+            {
+                foreach (var pair in _places)
+                {
+                    var node = pair.Value;
+                    if (0 == node.children.Count)
+                    {
+                        _sinkPlaceId = pair.Key;
+                        break;
+                    }
+                }
+            }
+
+            return _sinkPlaceId;
+        }
+
+
+
+        public int GetHighestPlaceId()
+        {
+            return _places.Keys.Max();
+        }
+
+        public int GetLowestTransitionId()
+        {
+            return _transitions.Keys.Min();
         }
 
         public void AddPlace(int id)
@@ -178,6 +213,16 @@ namespace ProcessMining
             return _transitions[transitionId].children.Select(node => node.id).ToList();
         }
 
+        public List<int> GetIdsOfChildTransitions(int placeId)
+        {
+            return _places[placeId].children.Select(node => node.id).ToList();
+        }
+
+        public List<int> GetIdsOfParentTransitions(int placeId)
+        {
+            return _places[placeId].parents.Select(node => node.id).ToList();
+        }
+
         public void InitializeTokens()
         {
             foreach (var place in _places.Values)
@@ -185,6 +230,90 @@ namespace ProcessMining
                 place.token = 0;
             }
             AddMarking(1);
+        }
+
+        class CostPath
+        {
+            public int cost;
+            public List<int> path;
+
+            public CostPath()
+            {
+                cost = 0;
+                path = new List<int>();
+            }
+        }
+
+        public int GetCost(int from, int to)
+        {
+            return 1;
+        }
+
+        // Not tested
+        public int GetShortestPath(int from, int to, Dictionary<int, List<int>> cost_dict)
+        {
+            var queue = new PriorityQueue<Int32,Int32>();
+
+            var minimum = this.GetLowestTransitionId();
+            var maximum = this.GetHighestPlaceId();
+
+            Dictionary<int,CostPath> dist = new Dictionary<int,CostPath>();
+
+            for (int i = minimum; i < maximum; i++)
+            {
+                if (0 == i)
+                {
+                    continue;
+                }
+
+                dist[i] = (new CostPath(){cost = Int32.MaxValue});
+            }
+
+            var minPath = dist[from].path;
+            minPath.Add(from);
+            dist[from] = new CostPath(){cost = 0, path = minPath};
+
+            HashSet<int> visited = new HashSet<int>();
+
+
+            queue.Enqueue(0,from);
+            while (!queue.IsEmpty)
+            {
+                var element = queue.Dequeue();
+                var id = element.Value;
+
+                visited.Add(id);
+
+                List<int> elements = null;
+                if (id > 0)
+                {
+                    //transitions are with negative ids
+                    elements = GetIdsOfChildTransitions(id);
+                }
+                else
+                {
+                    elements = GetIdsOfChildPlaces(id);
+                }
+
+                foreach (var elementId in elements)
+                {
+                    if (!visited.Contains(elementId))
+                    {
+                        var costSoFar = element.Key + GetCost(id, elementId);
+
+                        var pathSoFar = dist[id].path;
+                        pathSoFar.Add(elementId);
+
+                        dist[elementId] = new CostPath() { cost = costSoFar, path = pathSoFar };
+
+                        queue.Enqueue(costSoFar, elementId);
+
+                    }
+                }
+
+            }
+
+            return dist[to].cost;
         }
     }
 }
